4d909c3f98d7ca57112a46896e5d0a8b9979b60d
hawri <hawri@DESKTOP-BMM1386>
2024-04-15 17:22:47 +0300

    Add project files.

---
+++ Search/main.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 8;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	//Повторы
+	for (int i = 0; i < n; i++)
+	{
+		bool met_before = false;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] == arr[i])
+			{
+				met_before = true;
+				break;
+			}
+		}
+		if (met_before)continue;
+		int counter = 0; //счетчик
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[i] == arr[j])counter++;
+		}
+		if (counter)cout << "Значение " << arr[i] << " повторяется " << counter << " раз" << endl;
+	}
+}
